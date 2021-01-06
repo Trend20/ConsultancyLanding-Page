56b06c75f3dc715acfd172925c9372455c89ce1d
devenock <enockomondi305@gmail.com>
2021-01-06 14:31:37 +0300

    making the team section  responsive

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -15,10 +15,12 @@ export default function Blog() {
             </div>
 
             <div className="blog-members">
-            <i className="fas fa-angle-left"></i>
+               <i className="fas fa-angle-left"></i>
 
                 <div className="blog-details">
-                <img src="/img/project.webp" alt="project1"/>
+                <div className="blog-image">
+                   <img src="/img/project.webp" alt="project4"/>
+                   </div>
                     <div className="author">
                        <img src="/img/team1.webp" alt="member1"/>
                        <p>Date: 01 Sept 2020</p>
@@ -27,7 +29,9 @@ export default function Blog() {
                 </div>
 
                 <div className="blog-details">
-                <img src="/img/project.webp" alt="project2"/>
+                <div className="blog-image">
+                   <img src="/img/project.webp" alt="project4"/>
+                   </div>
                     <div className="author">
                       <img src="/img/team2.webp" alt="member1"/>
                       <p>Date: 01 Oct 2020</p>
@@ -36,7 +40,9 @@ export default function Blog() {
                 </div>
 
                 <div className="blog-details">
-                <img src="/img/project.webp" alt="project3"/>
+                <div className="blog-image">
+                   <img src="/img/project.webp" alt="project4"/>
+                   </div>
                     <div className="author">
                       <img src="/img/team3.webp" alt="member1"/>
                       <p>Date: 01 Nov 2020</p>
@@ -45,7 +51,9 @@ export default function Blog() {
                 </div>
 
                 <div className="blog-details">
-                <img src="/img/project.webp" alt="project4"/>
+                   <div className="blog-image">
+                   <img src="/img/project.webp" alt="project4"/>
+                   </div>
                     <div className="author">
                       <img src="/img/team4.webp" alt="project4"/>
                       <p>Date: 01 Dec 2020</p>
